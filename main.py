e99ce9db970e22c870bcdf0bcb3e717c7153fa89
pinkrach <166764867+pinkrach@users.noreply.github.com>
2025-04-10 13:26:56 -0600

    Changes to Main
    import statement

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 # Rachel Pinkney, Karlie Ward, Sabrina Wong, Spencer Bigelow, Mason Zarges, Gavin Smith
 # GROUP PROJECT WOHOOOO!! :)
+
+import sqlalchemy
+import pandas as pd
+import matplotlib.pyplot as plot
+
+#optional to use
+import openpyxl
+from sqlalchemy sql import text
+import psycopg2
+
+
