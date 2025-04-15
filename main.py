d1573fa9c55efb93d01145f2d6c1cead30b7427b
pinkrach <166764867+pinkrach@users.noreply.github.com>
2025-04-14 18:46:10 -0600

    Add Excel
    Adding the Excel file

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ import matplotlib.pyplot as plot
 
 #optional to use
 import openpyxl
-from sqlalchemy sql import text
+from sqlalchemy import text
 import psycopg2
 
+df = pd.read_excel('Retail_Sales_Data.xlsx')
 
+print("If you want to import data, enter 1." "If you want to see summaries of stored data, enter 2. Enter any other value to exit the program: ")
